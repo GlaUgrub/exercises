b8106d1470bdc3c4c887b3b8eb2909262a134553
Stanislav Efremov <efremov.stanislav.v@gmail.com>
2016-11-27 16:38:48 +0300

    [U2.0] main() was modified to support execution of many exercises. Exercize 4.3 was implemented.
    Signed-off-by: Stanislav Efremov <efremov.stanislav.v@gmail.com>

--- Stroustrup/stroustrup.cpp
+++ Stroustrup/stroustrup.cpp
@@ -3,8 +3,60 @@
 #include <iostream>
 #include "stroustrup.h"
 
-int main()
+void PrintSizesOfTypes()
+{
+    std::cout << "Size of char        = " << sizeof(char) << "\n";
+    std::cout << "Size of short       = " << sizeof(short) << "\n";
+    std::cout << "Size of int         = " << sizeof(int) << "\n";
+    std::cout << "Size of long        = " << sizeof(long) << "\n";
+    std::cout << "Size of float       = " << sizeof(float) << "\n";
+    std::cout << "Size of double      = " << sizeof(double) << "\n";
+    std::cout << "Size of long double = " << sizeof(long double) << "\n";
+    std::cout << "Size of bool        = " << sizeof(char) << "\n";
+    std::cout << "Size of User        = " << sizeof(User) << "\n";
+    std::cout << "Size of Beer        = " << sizeof(Beer) << "\n";
+    std::cout << "Size of int*        = " << sizeof(int*) << "\n";
+    std::cout << "Size of size_t*     = " << sizeof(size_t) << "\n";
+
+}
+
+void HelloWorld()
 {
     std::cout << "Hello world" << '\n';
+}
+
+int main()
+{
+    bool incorrectInput = true;
+    int exerciseNumber = 0;
+
+    while (incorrectInput == true)
+    {
+        std::cout << "Please enter exercise number:";
+
+        std::cin >> exerciseNumber;
+        std::cout << '\n';
+        if (exerciseNumber < 41 || int(exerciseNumber) > 250) // out of valid range
+        {
+            std::cout << "Incorrect exercise number. Please reenter.\n";
+            continue;
+        }
+
+        switch (exerciseNumber)
+        {
+        case 41:
+            HelloWorld();
+            break;
+        case 43:
+            PrintSizesOfTypes();
+            break;
+        default:
+            std::cout << "Incorrect exercise number. Please reenter.\n";
+            continue;
+        }
+
+        incorrectInput = false;
+    }
+
     return 0;
 }
--- Stroustrup/stroustrup.h
+++ Stroustrup/stroustrup.h
@@ -1,1 +1,64 @@
 /* First training header file for Stroustup exercises*/
+
+#include <string>
+#include <complex>
+
+char ch; // definition
+
+std::string s; // definition
+
+int count = 1; // definition
+
+const double pi = 3.14; // definition
+
+extern int error_number; // declaration only
+int error_number; // definition
+
+const char* name = "Me"; // definition
+const char* season[] = {"spring", "summer", "autumn", "winter"}; // definition
+
+struct Date // definition
+{
+    int d, m, y;
+};
+
+int day(Date* p) // definition
+{
+    return p->d;
+}
+
+double SqrtMy(double); // declaration only
+double SqrtMy(double in) // definition
+{
+    return in * in;
+}
+
+template<class T> T abs(T a) // definition
+{
+    return a >= 0 ? a : -a;
+}
+
+typedef std::complex<short> Point; // definition
+
+struct User; // declaration only
+struct User // definition
+{
+    std::string name;
+    std::string surname;
+    std::string login;
+    std::string password;
+    std::string cellNumber;
+    unsigned int id;
+};
+
+enum Beer // definition
+{
+    Carlsberg,
+    Tuborg,
+    Thor
+};
+
+namespace NS  // definition
+{
+    int a;
+}
