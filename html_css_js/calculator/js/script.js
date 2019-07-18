21fe92e7cff900faeaae3ebb8455e675718475c9
glaugrub <efremov.stanislav.v@gmail.com>
2019-07-18 20:51:06 +0300

    [html css js] Add load of defaults using object constructor
    Also small refactoring
    
    Signed-off-by: glaugrub <efremov.stanislav.v@gmail.com>

--- html_css_js/calculator/js/script.js
+++ html_css_js/calculator/js/script.js
@@ -7,11 +7,19 @@ document.
     addEventListener("DOMContentLoaded",
 		     function (event) {
 
+			 function Data() {
+			     this.years = 4;
+			     this.init_summ = 15000000;
+			     this.capital_increase = 10;
+			     this.init_salary = 2500000;
+			     this.salary_increase = 7;
+			 }
+
 			 function setNumberById(id, number) {
 			     document.querySelector(id).value = number;
 			 }
 
-			 function applyDefaults(defaults) {
+			 function fillDefaults(defaults) {
 			     setNumberById("#years", defaults.years);
 			     setNumberById("#init_summ", defaults.init_summ);
 			     setNumberById("#capital_increase", defaults.capital_increase);
@@ -19,39 +27,48 @@ document.
 			     setNumberById("#salary_increase", defaults.salary_increase);
 			 }
 
-			 ajaxUtils
-			     .sendGetRequest("data/defaults.json", applyDefaults);
+			 var init_by_ajax = false;
+			 var data = new Data();
+
+			 if (init_by_ajax) {
+			     ajaxUtils
+				 .sendGetRequest("data/defaults.json", fillDefaults);
+			 }
+			 else {
+			     fillDefaults(data);
+			 }			     
 
 			 function parseNumberById(id) {
 			     return Number(document.querySelector(id).value);
 			 }
 
+			 function parseData() {
+			     data.years = parseNumberById("#years");
+     		     	     data.init_summ = parseNumberById("#init_summ");
+                             data.capital_increase = parseNumberById("#capital_increase");
+			     data.init_salary = parseNumberById("#init_salary");
+			     data.salary_increase = parseNumberById("#salary_increase");
+			 }
 
- 		     	 function calculate(event) {
-			     var years = parseNumberById("#years");
-
-			     var given = {};
-		     	     given.init_summ = parseNumberById("#init_summ");
-                             given.capital_increase = parseNumberById("#capital_increase");
-			     given.init_salary = parseNumberById("#init_salary");
-			     given.salary_increase = parseNumberById("#salary_increase");
-
-			     var summ = given.init_summ;
-			     var salary = given.init_salary;
+ 		     	 function calculate() {
+			     parseData();
+			     
+			     var summ = data.init_summ;
+			     var salary = data.init_salary;
 
 			     yearly_results = [];
 
-			     for (var i = 0; i < years; i++) {				 
-				 summ = add_percent(summ, given.capital_increase);
+			     for (var i = 0; i < data.years; i++) {				 
+				 summ = add_percent(summ, data.capital_increase);
 				 summ += salary;
-				 salary = add_percent(salary, given.salary_increase);
+				 salary = add_percent(salary, data.salary_increase);
 				 yearly_results[i] = summ;
 			     }
 
 			     return yearly_results;
 		     	 }
 
-			 function outputResults(event, yearly) {
+			 function outputResults(yearly) {
 			     var out_region = document.querySelector("#output");
 			     out_region.innerHTML = "<p>Результат:</p>";
 
@@ -62,8 +79,8 @@ document.
 			 }
 			 
 			 function doWork(event) {
-			     results = calculate(event);
-			     outputResults(event, results);
+			     results = calculate();
+			     outputResults(results);
 			 }
 
 			 document.querySelector("button").addEventListener("click", doWork);
