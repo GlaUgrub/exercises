82b08265679fbd2f4f7cdc0b8e18b8015ed76680
glaugrub <efremov.stanislav.v@gmail.com>
2019-07-31 21:30:37 +0300

    [html css js] Untabify
    Signed-off-by: glaugrub <efremov.stanislav.v@gmail.com>

--- html_css_js/calculator/js/script.js
+++ html_css_js/calculator/js/script.js
@@ -5,96 +5,96 @@ function add_percent(value, perc) {
 
 document.
     addEventListener("DOMContentLoaded",
-		     function (event) {
+                     function (event) {
 
-			 var output = document.querySelector("#output");
-			 output.style.display = "none";
+                         var output = document.querySelector("#output");
+                         output.style.display = "none";
 
-			 function Data() {
-			     this.years = 4;
-			     this.init_summ = 15000000;
-			     this.capital_increase = 10;
-			     this.init_salary = 350000;
-			     this.salary_increase = 7;
-			 }			 
+                         function Data() {
+                             this.years = 4;
+                             this.init_summ = 15000000;
+                             this.capital_increase = 10;
+                             this.init_salary = 350000;
+                             this.salary_increase = 7;
+                         }                       
 
-			 function setNumberById(id, number) {
-			     document.querySelector(id).value = number;
-			 }
+                         function setNumberById(id, number) {
+                             document.querySelector(id).value = number;
+                         }
 
-			 function fillDefaults(defaults) {
-			     setNumberById("#years", defaults.years);
-			     setNumberById("#init_summ", defaults.init_summ);
-			     setNumberById("#capital_increase", defaults.capital_increase);
-			     setNumberById("#init_salary", defaults.init_salary);
-			     setNumberById("#salary_increase", defaults.salary_increase);
-			 }
+                         function fillDefaults(defaults) {
+                             setNumberById("#years", defaults.years);
+                             setNumberById("#init_summ", defaults.init_summ);
+                             setNumberById("#capital_increase", defaults.capital_increase);
+                             setNumberById("#init_salary", defaults.init_salary);
+                             setNumberById("#salary_increase", defaults.salary_increase);
+                         }
 
-			 var init_by_ajax = false;
-			 var data = new Data();
+                         var init_by_ajax = false;
+                         var data = new Data();
 
-			 if (init_by_ajax) {
-			     ajaxUtils
-				 .sendGetRequest("data/defaults.json", fillDefaults);
-			 }
-			 else {
-			     fillDefaults(data);
-			 }			     
+                         if (init_by_ajax) {
+                             ajaxUtils
+                                 .sendGetRequest("data/defaults.json", fillDefaults);
+                         }
+                         else {
+                             fillDefaults(data);
+                         }                           
 
-			 function parseNumberById(id) {
-			     return Number(document.querySelector(id).value);
-			 }
+                         function parseNumberById(id) {
+                             return Number(document.querySelector(id).value);
+                         }
 
-			 function parseData() {
-			     data.years = parseNumberById("#years");
-     		     	     data.init_summ = parseNumberById("#init_summ");
+                         function parseData() {
+                             data.years = parseNumberById("#years");
+                             data.init_summ = parseNumberById("#init_summ");
                              data.capital_increase = parseNumberById("#capital_increase");
-			     var salary = parseNumberById("#init_salary");
-			     var salary_term = document.querySelector("#salary_term").value;
-			     if (salary_term === "month") {
-				 data.init_salary = salary * 12;
-			     }
-			     else {
-				 data.init_salary = salary;
-			     }
-			     data.salary_increase = parseNumberById("#salary_increase");
-			 }
+                             var salary = parseNumberById("#init_salary");
+                             var salary_term = document.querySelector("#salary_term").value;
+                             if (salary_term === "month") {
+                                 data.init_salary = salary * 12;
+                             }
+                             else {
+                                 data.init_salary = salary;
+                             }
+                             data.salary_increase = parseNumberById("#salary_increase");
+                         }
 
- 		     	 function calculate() {
-			     parseData();
-			     
-			     var summ = data.init_summ;
-			     var salary = data.init_salary;
+                         function calculate() {
+                             parseData();
+                             
+                             var summ = data.init_summ;
+                             var salary = data.init_salary;
 
-			     yearly_results = [];
+                             yearly_results = [];
 
-			     for (var i = 0; i < data.years; i++) {
-				 summ = add_percent(summ, data.capital_increase);
-				 summ += salary;
-				 salary = add_percent(salary, data.salary_increase);
-				 yearly_results[i] = summ;
-			     }
+                             for (var i = 0; i < data.years; i++) {
+                                 summ = add_percent(summ, data.capital_increase);
+                                 summ += salary;
+                                 salary = add_percent(salary, data.salary_increase);
+                                 yearly_results[i] = summ;
+                             }
 
-			     return yearly_results;
-		     	 }
+                             return yearly_results;
+                         }
 
-			 function outputResults(yearly) {
-			     var out_region = document.querySelector("#output");
-			     out_region.innerHTML = "<p>Результат:</p>";
+                         function outputResults(yearly) {
+                             var out_region = document.querySelector("#output");
+                             out_region.innerHTML = "<p>Результат:</p>";
 
-			     for (var i = 0; i < yearly.length; i++) {
-				 var line = "Год " + (i + 1) + ": " + yearly[i];
-				 out_region.innerHTML += "<p>" + line + "</p>";
-			     }
-			 }
+                             for (var i = 0; i < yearly.length; i++) {
+                                 var line = "Год " + (i + 1) + ": " + yearly[i];
+                                 out_region.innerHTML += "<p>" + line + "</p>";
+                             }
+                         }
 
-			 function doWork(event) {
-			     results = calculate();
-			     outputResults(results);
-			     output.style.display = "block";
-			 }
+                         function doWork(event) {
+                             results = calculate();
+                             outputResults(results);
+                             output.style.display = "block";
+                         }
 
-			 document.querySelector("button").addEventListener("click", doWork);
-		     }
+                         document.querySelector("button").addEventListener("click", doWork);
+                     }
 
-		    )
+                    )
